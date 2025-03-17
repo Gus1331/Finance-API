3996d53b8a65c99fe8492dd015a0667dbb3a9fa3
Gus1331 <gustavooliver.souza@gmail.com>
2025-03-17 14:21:23 -0300

    hotfix: schedule interval, indent fixes

--- api/controllers/awesomeapi_controller.py
+++ api/controllers/awesomeapi_controller.py
@@ -1,6 +1,6 @@
 import requests
 from uuid import uuid4
-
+from datetime import datetime
 from config import conect_elastic
 from config import create_app
 
@@ -16,25 +16,24 @@ def leitura():
 
         data = response.json()
 
-        moedas = {'moedas': [
+        moedas = [
             {
                 'nome': data['USDBRL']['name'],
                 'valor': data['USDBRL']['high'],
-                'dtLeitura': data['USDBRL']['create_date']
+                'dtLeitura': datetime.strptime(data['USDBRL']['create_date'], "%Y-%m-%d %H:%M:%S")
             },
             {
                 'nome': data['CADBRL']['name'],
                 'valor': data['CADBRL']['high'],
-                'dtLeitura': data['CADBRL']['create_date']
+                'dtLeitura': datetime.strptime(data['CADBRL']['create_date'], "%Y-%m-%d %H:%M:%S")
             },
             {
                 'nome': data['EURBRL']['name'],
                 'valor': data['EURBRL']['high'],
-                'dtLeitura': data['EURBRL']['create_date']
+                'dtLeitura': datetime.strptime(data['EURBRL']['create_date'], "%Y-%m-%d %H:%M:%S")
             },
-        ]}
-        
-        print('Cotações registradas!')
-        es.index(index='moedas', id=str(uuid4()), document=moedas) 
+        ]
+        for moeda in moedas:
+            es.index(index='moedas', id=str(uuid4()), document=moeda) 
     except Exception as e:
         print(f'Erro ao se comunicar com awesomeAPI: {e}')
